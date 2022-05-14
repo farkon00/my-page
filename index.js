5a026d5b3c9cc36a5589155e8f3c4e8de712b643
farkon00 <sammer2016sammer@gmail.com>
2022-05-14 13:28:19 +0300

    Basic functiionality for tetris animation added

--- index.js
+++ index.js
@@ -1,2 +1,83 @@
 "use strict";
-console.log("Hello world");
+let FPS = 3;
+let gridWidth = 6;
+var canvas;
+var ctx;
+var canvasSize;
+var cellSize;
+let figures = [
+    [
+        [[0, 1], [1, 1], [2, 1], [3, 1]],
+        [[2, 0], [2, 1], [2, 2], [2, 3]],
+        [[0, 2], [1, 2], [2, 2], [3, 2]],
+        [[1, 0], [1, 1], [1, 2], [1, 3]]
+    ],
+    [
+        [[0, 0], [0, 1], [1, 1], [2, 1]],
+        [[2, 0], [1, 0], [1, 1], [1, 2]],
+        [[0, 1], [1, 1], [2, 1], [2, 2]],
+        [[1, 0], [1, 1], [1, 2], [0, 2]]
+    ],
+    [
+        [[0, 1], [1, 1], [2, 1], [2, 0]],
+        [[1, 0], [1, 1], [1, 2], [2, 2]],
+        [[0, 2], [0, 1], [1, 1], [2, 1]],
+        [[0, 0], [1, 0], [1, 1], [1, 2]]
+    ],
+    [
+        [[1, 0], [2, 0], [1, 1], [2, 1]]
+    ],
+    [
+        [[0, 1], [1, 1], [1, 0], [2, 0]],
+        [[1, 0], [1, 1], [2, 1], [2, 2]],
+        [[0, 2], [1, 2], [1, 1], [2, 1]],
+        [[0, 0], [0, 1], [1, 1], [1, 2]]
+    ],
+    [
+        [[0, 1], [1, 1], [2, 1], [1, 0]],
+        [[1, 0], [1, 1], [1, 2], [2, 1]],
+        [[0, 1], [1, 1], [2, 1], [1, 2]],
+        [[1, 0], [1, 1], [1, 2], [0, 1]]
+    ],
+    [
+        [[0, 0], [1, 0], [1, 1], [2, 1]],
+        [[2, 0], [2, 1], [1, 1], [1, 2]],
+        [[0, 1], [1, 1], [1, 2], [2, 2]],
+        [[1, 0], [1, 1], [0, 1], [0, 2]]
+    ]
+];
+let colors = ["#00ffff", "blue", "orange", "yellow", "green", "purple", "red"];
+function drawGrid() {
+    for (let i = 1; i < gridWidth; i++) {
+        ctx.fillStyle = "gray";
+        ctx.fillRect(i * cellSize, 0, 1, canvasSize);
+        ctx.fillRect(0, i * cellSize, canvasSize, 1);
+    }
+}
+function drawBlock(cell) {
+    if (!cell.filled) {
+        return;
+    }
+    ctx.fillStyle = colors[cell.color - 1];
+    ctx.fillRect(cell.x * cellSize, cell.y * cellSize, cellSize, cellSize);
+}
+function runAnimationLoop() {
+    ctx.clearRect(0, 0, canvasSize, canvasSize);
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvasSize, canvasSize);
+    drawGrid();
+    drawBlock({ x: 1, y: 1, color: 1, filled: true });
+    drawBlock({ x: 1, y: 2, color: 2, filled: true });
+    drawBlock({ x: 2, y: 1, color: 3, filled: true });
+    drawBlock({ x: 2, y: 2, color: 4, filled: true });
+    drawBlock({ x: 3, y: 1, color: 5, filled: true });
+    drawBlock({ x: 1, y: 3, color: 6, filled: true });
+}
+function updateCanvas() {
+    canvas = document.getElementById("tetris-canvas");
+    ctx = canvas.getContext("2d");
+    canvasSize = canvas.width;
+    cellSize = canvasSize / gridWidth;
+}
+updateCanvas();
+setInterval(runAnimationLoop, 1000 / FPS);
--- index.ts
+++ index.ts
@@ -1,1 +1,93 @@
-console.log("Hello world");
+type Cell = {
+    x : number,
+    y : number,
+    color : number
+}
+
+let FPS: number = 3;
+let gridWidth: number = 6;
+
+var canvas: HTMLCanvasElement;
+var ctx: CanvasRenderingContext2D;
+var canvasSize: number;
+var cellSize: number;
+
+let figures = [
+    [
+        [[0, 1], [1, 1], [2, 1], [3, 1]],
+        [[2, 0], [2, 1], [2, 2], [2, 3]],
+        [[0, 2], [1, 2], [2, 2], [3, 2]],
+        [[1, 0], [1, 1], [1, 2], [1, 3]]
+    ],
+    [
+        [[0, 0], [0, 1], [1, 1], [2, 1]],
+        [[2, 0], [1, 0], [1, 1], [1, 2]],
+        [[0, 1], [1, 1], [2, 1], [2, 2]],
+        [[1, 0], [1, 1], [1, 2], [0, 2]]
+    ],
+    [
+        [[0, 1], [1, 1], [2, 1], [2, 0]],
+        [[1, 0], [1, 1], [1, 2], [2, 2]],
+        [[0, 2], [0, 1], [1, 1], [2, 1]],
+        [[0, 0], [1, 0], [1, 1], [1, 2]]
+    ],
+    [
+        [[1, 0], [2, 0], [1, 1], [2, 1]]
+    ],
+    [
+        [[0, 1], [1, 1], [1, 0], [2, 0]],
+        [[1, 0], [1, 1], [2, 1], [2, 2]],
+        [[0, 2], [1, 2], [1, 1], [2, 1]],
+        [[0, 0], [0, 1], [1, 1], [1, 2]]
+    ],
+    [
+        [[0, 1], [1, 1], [2, 1], [1, 0]],
+        [[1, 0], [1, 1], [1, 2], [2, 1]],
+        [[0, 1], [1, 1], [2, 1], [1, 2]],
+        [[1, 0], [1, 1], [1, 2], [0, 1]]
+    ],
+    [
+        [[0, 0], [1, 0], [1, 1], [2, 1]],
+        [[2, 0], [2, 1], [1, 1], [1, 2]],
+        [[0, 1], [1, 1], [1, 2], [2, 2]],
+        [[1, 0], [1, 1], [0, 1], [0, 2]]
+    ]
+]
+
+let colors: Array<string> = ["#00ffff", "blue", "orange", "yellow", "green", "purple", "red"];
+
+function drawGrid(): void {
+    for (let i=1;i<gridWidth;i++) {
+        ctx.fillStyle = "gray";
+        ctx.fillRect(i*cellSize, 0, 1, canvasSize);
+        ctx.fillRect(0, i*cellSize, canvasSize, 1);
+    }
+}
+
+function drawBlock(cell: Cell): void {
+    ctx.fillStyle = colors[cell.color];
+    ctx.fillRect(cell.x * cellSize, cell.y * cellSize, cellSize, cellSize);
+}
+
+function runAnimationLoop(): void {
+    ctx.clearRect(0, 0, canvasSize, canvasSize);
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvasSize, canvasSize);
+    drawGrid();
+    drawBlock({x : 1, y : 1, color : 1});
+    drawBlock({x : 1, y : 2, color : 2});
+    drawBlock({x : 2, y : 1, color : 3});
+    drawBlock({x : 2, y : 2, color : 4});
+    drawBlock({x : 3, y : 1, color : 5});
+    drawBlock({x : 1, y : 3, color : 6});
+}
+
+function updateCanvas(): void {
+    canvas = <HTMLCanvasElement>document.getElementById("tetris-canvas")
+    ctx = <CanvasRenderingContext2D>canvas.getContext("2d");
+    canvasSize = canvas.width;
+    cellSize = canvasSize / gridWidth;
+}
+
+updateCanvas();
+setInterval(runAnimationLoop, 1000 / FPS);
